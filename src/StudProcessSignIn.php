9085226ac954ffb0430513ca200b667c7d110ef6
supthunder <ejohn3@umbc.edu>
2015-11-18 16:37:13 -0500

    added comments

--- src/StudProcessSignIn.php
+++ src/StudProcessSignIn.php
@@ -14,6 +14,8 @@ $lastName = strtoupper($_POST["lastN"]);
 $email = $_POST["email"];
 
 //$sql = "select * from Proj2Students where `FirstName` = `$firstName` AND `LastName` = `$lastName` AND `Email` = `$email`";
+
+// This will get the userId, which is unique for each student, into session
 $_SESSION["userId"] = "SELECT `id` FROM `Proj2Advisors` WHERE `FirstName` = '$firstName' AND `LastName` = '$lastName' AND `Email` = '$email'";
 
 
