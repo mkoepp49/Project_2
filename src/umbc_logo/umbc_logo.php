39ca69cce8b412b457dff1fb95e5ac3d5dc11a91
mkoepp49 <monicak1@umbc.edu>
2015-11-28 17:23:46 -0500

    Adding new images for CSS/HTML of webpages

---
+++ src/umbc_logo/umbc_logo.php
@@ -0,0 +1,10 @@
+<br>
+<br>
+	<img src="./images/umbcLogo.png" border="0" height="90" width="90" style="float:left;", title="UMBC ROCKS!"/>
+<br>
+<br>
+
+		<!--
+		<div class="photo">
+			<img src="./images/umbc_decal.jpg" alt="umbc_dog" style="width:90px;height:90px;" />
+		</div>  -->
